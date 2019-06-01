1afe91270fad4d17688857e9d46a5d0210130014
Julien Piccaluga <julien.piccaluga@gmail.com>
2019-06-01 22:31:04 +0200

    Update error handling

--- src/main/kotlin/kx/io/dashboard/gitlab/GitlabPipelineService.kt
+++ src/main/kotlin/kx/io/dashboard/gitlab/GitlabPipelineService.kt
@@ -1,11 +1,17 @@
 package kx.io.dashboard.gitlab
 
+import mu.KotlinLogging
 import java.util.regex.Pattern
 
 class GitlabPipelineService(private val gitlabClient: GitlabClient) {
 
+    private val log = KotlinLogging.logger { }
+
     fun lastPipeLineStatus(projectId: String, branchName: String) : Status {
         return gitlabClient.fetchPipelines(projectId, 100, Sort.DESC)
+                .onErrorContinue { t: Throwable?, u: Any? ->
+                    log.error("Could not retrieve from repo $branchName ($projectId)", t)
+                }
                 .filter{ Pattern.matches(branchName, it.ref)}
                 .map { it.status }
                 .map {
